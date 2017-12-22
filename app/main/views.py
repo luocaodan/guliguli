a620dfa44709b09951160c9500b2ef3b6e837b2d
Malxi <13987610292@sina.cn>
2017-12-22 15:02:41 +0800

    add user to main.home_page

--- app/main/views.py
+++ app/main/views.py
@@ -2,6 +2,7 @@ from flask import render_template, session, redirect, url_for,flash, current_app
 from datetime import datetime
 from . import main
 from .forms import NameForm
+from flask_login import login_user, login_required, logout_user, current_user
 #from .. import db
 #from ..models import User
 from ..email import send_email
@@ -12,4 +13,4 @@ import datetime
 
 @main.route('/')
 def home_page():
-    return render_template('index.html')
+    return render_template('index.html', user=current_user)
