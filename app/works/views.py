c1201e40fd598758dffca35479f758980676e3c3
Malxi <13987610292@sina.cn>
2017-12-23 21:54:52 +0800

    add uploads

--- app/works/views.py
+++ app/works/views.py
@@ -1,13 +1,56 @@
-from flask import render_template, session, redirect, url_for,flash, current_app
+from flask import render_template, session, redirect, url_for,flash, current_app, request
 from datetime import datetime
 from . import works
 from flask_login import login_user, login_required, logout_user, current_user
-#from .. import db
+from werkzeug import secure_filename
+from .. import db
+from .. import photos
 #from ..models import User
 from ..email import send_email
-#from .. import mongo
+import PIL
+from PIL import Image
+import traceback
 import random
+import os
+
+def gen_file_name(filename):
+    '''
+    If file exits, then gen a new filename
+    '''
+    i = 1
+
+    while os.path.exists(os.path.join(current_app.config['UPLOAD_FOLDER'], filename)):
+        name, extension = os.path.splitext(filename)
+        filename = '%s_%s%s' % (name, str(i), extension)
+        i += 1
+    
+    return filename
+
+def create_thumbnail(image):
+    try:
+        base_width = 80
+        img = Image.open(os.path.join(current_app.config['UPLOADED_PHOTOS_DEST'], image))
+        w_percent = (base_width / float(img.size[0]))
+        h_size = int((float(img.size[1]) * float(w_percent)))
+        img = img.resize((base_width, h_size), PIL.Image.ANTIALIAS)
+        img.save(os.path.join(current_app.config['THUMBNAIL_FOLDER'], image))
+
+        return True
+
+    except:
+        print traceback.format_exc()
+        return False
 
 @works.route('/')
-def post():
+def worksPage():
     return render_template('works/post.html')
+
+@works.route('/post', methods = ['GET', 'POST'])
+@login_required
+def post():
+    if request.method == 'POST':
+        if 'file' in request.files:
+            filename = photos.save(request.files['photo'])
+            create_thumbnail(filename)
+            return photos.url(filename)
+    render_template('works/post.html')
