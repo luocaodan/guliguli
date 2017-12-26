cac1ddf2319703723c5a444cdef43b6c04c3cc0c
Malxi <13987610292@sina.cn>
2017-12-26 17:48:59 +0800

    modified

--- procedure.sql
+++ procedure.sql
@@ -129,8 +129,8 @@ create procedure query_nworks(n int)
 create procedure query_usersworks(u_id int(8)) 
     begin
         select * from works
-        order by worksid desc
-        where userid=u_id;
+        where userid=u_id
+        order by worksid desc;
     end//
 
 /*
@@ -175,7 +175,7 @@ create procedure query_comment(w_id int(8))
         select commentid, text, worksid, comment.userid, date_post, nickname, profile_photo, sex 
         from comment, users 
         where users.userid = comment.userid and worksid=w_id
-        order by commentid desc ;
+        order by commentid desc;
     end//
 
 /*
