81d00bc8d8d5a1997a3ed6824d528fd3cdeba3a1
Malxi <13987610292@sina.cn>
2017-12-29 20:56:28 +0800

    modified

---
+++ app/insertAc.py
@@ -0,0 +1,14 @@
+﻿# -*- coding: utf-8 -*-
+
+import requests
+
+def test():
+    data = {}
+    data['cont'] = u'二次元萌什么?'
+    data['d_release'] = '2018-1-1'
+    data['img'] = '/static/image/596loyx0vk.png'
+    r = requests.post('http://127.0.0.1:5000/api/insertActivity', data=data)
+    print r.text
+
+if __name__ == '__main__':
+    test()
